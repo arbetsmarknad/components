4e36515e8b86ea8035504eadaa946019dbc159ac
Henry Catalini Smith <henry@catalinismith.se>
2025-01-22 21:49:46 +0100

    Add classNames to container

--- Container.tsx
+++ Container.tsx
@@ -1,12 +1,16 @@
 import type { FC, ReactElement } from "react";
+import { cn } from "./utils";
 
 export type ContainerProps = {
   children: ReactElement | ReactElement[];
+  className?: string;
 };
 
-export const Container: FC<ContainerProps> = ({ children }) => {
+export const Container: FC<ContainerProps> = ({ children, className }) => {
   return (
-    <div className="flex w-full max-w-5xl flex-col px-4 lg:px-0">
+    <div
+      className={cn("flex w-full max-w-5xl flex-col px-4 lg:px-0", className)}
+    >
       {children}
     </div>
   );
