3b2f2bde2338ef02ff9180065575d813a834f368
Henry Catalini Smith <henry@catalinismith.se>
2025-01-25 07:48:46 +0100

    Add section + section heading

---
+++ SectionHeading.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { SectionHeading } from "./SectionHeading";
+
+export default {
+  title: "SectionHeading",
+  component: SectionHeading,
+};
+
+export const Default = () => <SectionHeading>Section Two</SectionHeading>;
