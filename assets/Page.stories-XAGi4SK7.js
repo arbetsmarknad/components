c5aa6ba3d1f35f1a4fac95aaf3db04ea5a6e3b8a
henrycatalinismith <henrycatalinismith@users.noreply.github.com>
2025-02-07 08:22:42 +0000

    deploy: f8854ec058d7a1cfa5813b1e34369bf1b202f81c

---
+++ assets/Page.stories-XAGi4SK7.js
@@ -0,0 +1,4 @@
+import{j as s}from"./jsx-runtime-CLpGMVip.js";const a=({children:r})=>s.jsx("div",{className:"flex flex-col items-center divide-y divide-solid divide-gray-200",children:r});a.__docgenInfo={description:"",methods:[],displayName:"Page"};const n={title:"Page",component:a},e=()=>s.jsxs(a,{children:[s.jsx("div",{children:"Example 1"}),s.jsx("div",{children:"Example 2"})]});e.__docgenInfo={description:"",methods:[],displayName:"Default"};var d,i,o;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`() => <Page>
+    <div>Example 1</div>
+    <div>Example 2</div>
+  </Page>`,...(o=(i=e.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,n as default};
